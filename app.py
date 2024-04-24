0e61c162031e84e2b3d65fda98a0619fe77c3f43
wangzhenyu <1904505316@qq.com>
2024-04-24 10:20:24 +0800

    上传helloworld

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+load_dotenv(find_dotenv('.flaskenv'), override=True)
+
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
+api = Api(app)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+class Helloworld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(Helloworld, '/')
 
 if __name__ == '__main__':
     app.run()
